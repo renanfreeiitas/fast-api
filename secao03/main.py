e32d3781cbf84e7d3ef7ef9c193673f0a462cc9e
renan <renanfreeiitas@gmail.com>
2023-04-27 18:06:50 +0100

    Criando secao03 e CRUD de cursos.

---
+++ secao03/main.py
@@ -0,0 +1,75 @@
+from fastapi import FastAPI
+from fastapi import HTTPException
+from fastapi import status
+
+from models import Curso
+
+
+app = FastAPI()
+
+
+cursos = {
+    1: {
+        "titulo": "Programacao para leigos",
+        "aulas": 112,
+        "horas": 58
+    },
+    2: {
+        "titulo": "Algoritmos e logica de programacao",
+        "aulas": 87,
+        "horas": 67
+    },
+    3: {
+        "titulo": "Python, Do zero ao Avancado",
+        "aulas": 150,
+        "horas": 30
+    }
+}
+
+
+@app.get("/cursos")
+async def get_cursos():
+    return cursos
+
+
+@app.get("/cursos/{curso_id}")
+async def get_curso(curso_id: int):
+    try:
+        curso = cursos[curso_id]
+        return curso
+    except KeyError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso nao encontrado.')
+
+
+@app.post("/cursos", status_code=status.HTTP_201_CREATED)
+async def post_curso(curso: Curso):
+    next_id: int = len(cursos) + 1
+    cursos[next_id] = curso
+    del curso.id
+    return curso
+
+
+@app.put("/cursos/{curso_id}", status_code=status.HTTP_200_OK)
+async def put_curso(curso_id: int, curso: Curso):
+    if curso_id in cursos:
+        cursos[curso_id] = curso
+        del curso.id
+        return curso
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Nao existe um curso com o ID {curso_id}")
+
+
+@app.delete("/cursos/{curso_id}", status_code=status.HTTP_200_OK)
+async def delete_curso(curso_id: int):
+    if curso_id in cursos:
+        curso = cursos[curso_id]
+        del cursos[curso_id]
+        return curso
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"O curso com o ID {curso_id} NAO existe.")
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, debug=True, reload=True)
